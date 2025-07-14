5da87648209ecaabb0f78c0b68a9419688ac578b
Dhruv Taraviya <dhruvtaraviya1@gmail.com>
2025-07-14 20:23:19 +0530

    Fix routing basename for GH Pages

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ function App() {
 
   return (
     // Router wrapper to handle navigation
-    <Router basename="/Recipe-Finder">
+    <Router basename="/">
       {/* Navigation bar visible on all pages */}
       <Nav />
 
